7a8ab880e5588ad8c825a30b7891b68621ee1c12
Karjun2004 <141245208+Karjun2004@users.noreply.github.com>
2023-08-29 12:23:48 +0530

    Update and rename arjun1(1).cpp to [FCFS].arjun1(1).cpp

--- [FCFS].arjun1(1).cpp
+++ [FCFS].arjun1(1).cpp
@@ -37,5 +37,4 @@ main()
   printf("\n\t P%d \t\t %d \t\t %d \t\t %d", p[i], bt[i], wt[i], tat[i]);
   printf("\nAverage Waiting Time -- %f", wtavg/n);
   printf("\nAverage Turnaround Time -- %f", tatavg/n); 
-  getch();
 }
